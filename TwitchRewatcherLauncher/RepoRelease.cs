582d3d7a628765edab8d49307083087cb8670ee8
TheDapperClam <thedapperclam@gmail.com>
2020-11-20 19:08:26 -0500

    Updated gitignore

--- TwitchRewatcherLauncher/RepoRelease.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Newtonsoft.Json;
-
-namespace TwitchRewatcherLauncher {
-    public class RepoRelease {
-        [JsonProperty("published_at")]
-        public DateTime PublishTime { get; set; }
-    }
-}
